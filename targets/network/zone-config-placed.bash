111bbabd2863b138c228d0139747f9e45e0f0c39
Phil Crockett <contact@philcrockett.com>
2026-03-24 09:13:32 -0500

    fix zone config placement

--- targets/network/zone-config-placed.bash
+++ targets/network/zone-config-placed.bash
@@ -4,10 +4,13 @@ REPO_PATH="${REPO_CONFIG_DIR}/firewalld/secure-lappy.xml"
 INSTALL_PATH=/etc/firewalld/zones/secure-lappy.xml
 
 satisfied_if() {
-  files_are_same "${INSTALL_PATH}" "${REPO_PATH}"
+  # we would use `files_are_same`, however `/etc/firewalld` is only readable by root and
+  # that would cause a `sudo` prompt
+  checkpoint_is_current "${REPO_PATH}"
 }
 
 apply() {
   as_root cp "${REPO_PATH}" "${INSTALL_PATH}"
   as_root firewall-cmd --reload
+  checkpoint_success
 }
